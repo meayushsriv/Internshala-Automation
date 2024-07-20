c3dd7221a2de276027165ae3165b51778018262a
Factve <me.facteverse@gmail.com>
2024-07-20 22:46:51 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    College: "Kalinga Institute Of Industrial Technology, Bhubaneshwar",
+    Degree: "Bachelor of Technology (B.Tech)",
+    Stream: "Computer Science & Engineering",
+    Percentage: "87.4",
+    Training: "Data Structures and Algorithms, Web Development",
+    Organization: "Kalinga Institute Of Industrial Technology",
+    description:
+      "Kalinga Institute Of Industrial Technology is a great institution where I got to learn the not only the basics of data structures and algorithms but I also got familiar with Web Development and Competitive Programming. Here, the main focus is on concept building and it was a great start for me.",
+    link: "https://github.com/meayushsriv",
+    hiringReason:
+      "Over the past few years, I have acquired relevant skills and experience, which I shall bring to your organization. I have also worked tirelessly on my communication abilities and teamwork skills, which I will put to use in my future career, which would be in your organization if I am selected for the position.",
+    availability: "Yes, I will be available",
+    rating: "4",
+  },
+];
---
+++ secret.js
@@ -0,0 +1,4 @@
+require("dotenv").config();
+let id = "process.env.ID";
+let pass = "process.env.PASS";
+module.exports = { id, pass };
