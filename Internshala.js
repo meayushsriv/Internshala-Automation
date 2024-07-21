c0f4da60261c9e8c355ed8c8adcf965546bd1d4d
Factve <me.facteverse@gmail.com>
2024-07-22 01:24:03 +0530

    final commit v2

--- Internshala.js
+++ Internshala.js
@@ -3,42 +3,89 @@ const loginLink = "https://internshala.com/login/student";
 
 require("dotenv").config();
 
-(async () => {
-  try {
-    const browser = await puppeteer.launch({
-      headless: false,
-      args: ["--start-maximized"],
-      defaultViewport: null,
-    });
-    const page = await browser.newPage();
-    await page.goto(loginLink);
-    await page.type("input[id='email']", process.env.EMAIL, { delay: 50 });
-    await page.type("input[id='password']", process.env.PASSWORD, {
+let browserOpen = puppeteer.launch({
+  headless: false,
+  args: ["--start-maximized"],
+  defaultViewport: null,
+});
+
+let page;
+
+browserOpen
+  .then(function (browserObj) {
+    let browserOpenPromise = browserObj.newPage(); // Open new page
+    return browserOpenPromise;
+  })
+  .then(function (newTab) {
+    page = newTab;
+    let internShalaOpenPromise = newTab.goto(loginLink); // Go to login page
+    return internShalaOpenPromise;
+  })
+  .then(function () {
+    return page.type("input[id='email']", process.env.EMAIL, { delay: 50 }); // Type email
+  })
+  .then(function () {
+    return page.type("input[id='password']", process.env.PASSWORD, {
+      delay: 50,
+    }); // Type password
+  })
+  .then(function () {
+    return page.click('button[id="login_submit"]', { delay: 40 }); // Click login button
+  })
+  .then(function () {
+    return page.waitForNavigation(); // Wait for navigation after login
+  })
+  .then(function () {
+    return waitAndClick('a[id="internships_new_superscript"]', page); // Click on internships link
+  })
+  .then(function () {
+    return waitAndClick('input[id="matching_preference"]', page); // Click on matching preference
+  })
+  .then(function () {
+    let allInternshipsPromise = page.$$(".internship_meta", {
       delay: 50,
     });
-    await page.click('button[id="login_submit"]', { delay: 40 });
-    await page.waitForNavigation(); // Ensure navigation is complete after login
-    await waitAndClick('a[id="internships_new_superscript"]', page);
-    await waitAndClick('input[id="matching_preference"]', page);
-  } catch (err) {
+    return allInternshipsPromise;
+  })
+  .then(function (internshipsArray) {
+    console.log("Number of internships : ", internshipsArray.length);
+    let applyInternship = internshipApply(page, internshipsArray[0]);
+  })
+  .catch(function (err) {
     console.error("Error: ", err);
-  }
-})();
+  });
 
-async function waitAndClick(selector, cPage) {
-  try {
-    await cPage.waitForSelector(selector);
-    await cPage.click(selector);
-  } catch (error) {
-    console.error("Error in waitAndClick:", error);
-  }
+function waitAndClick(selector, cPage) {
+  return new Promise(function (resolve, reject) {
+    cPage
+      .waitForSelector(selector)
+      .then(function () {
+        return cPage.click(selector);
+      })
+      .then(function () {
+        resolve();
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
 }
 
-async function waitAndType(selector, cPage, value) {
-  try {
-    await cPage.waitForSelector(selector);
-    await cPage.type(selector, value);
-  } catch (error) {
-    console.error("Error in waitAndType:", error);
-  }
+function internshipApply(page, selector) {
+  return new Promise((resolve, reject) => {
+    page
+      .evaluate((selector) => {
+        if (selector) {
+          selector.click();
+        } else {
+          throw new Error("Element not found");
+        }
+      }, selector)
+      .then(() => {
+        resolve();
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
